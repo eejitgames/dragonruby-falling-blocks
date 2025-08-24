3bf46a9b11adf3c14e1ac93c80384b7ecb7f76a6
eejit <eejitgames@gmail.com>
2025-08-24 14:47:05 +0100

    many updates, initial pattern matching

--- mygame/app/main.rb
+++ mygame/app/main.rb
@@ -18,13 +18,49 @@ GRID_WIDTH = 10
 GRID_HEIGHT = 10
 TILE_SIZE = 64
 
-RESOURCE_TYPES = [:seed, :soil, :water, :sun]
+RESOURCE_TYPES = [:seed, :soil, :water, :sun, :rock]
 
 GRID_PIXEL_WIDTH = GRID_WIDTH * TILE_SIZE
 GRID_PIXEL_HEIGHT = GRID_HEIGHT * TILE_SIZE
 
 GRID_OFFSET_X = ((GAME_WIDTH - GRID_PIXEL_WIDTH) / 2).floor
 GRID_OFFSET_Y = ((GAME_HEIGHT - GRID_PIXEL_HEIGHT) / 2).floor
+
+SHAPES = {
+  square: [
+    [[0,0],[1,0],[0,1],[1,1]]
+  ],
+  l_shape: [
+    [[0,0],[0,1],[0,2],[1,2]],
+    [[0,1],[1,1],[2,1],[2,0]],
+    [[0,0],[1,0],[1,1],[1,2]],
+    [[0,0],[0,1],[1,0],[2,0]]
+  ],
+  reverse_l: [
+    [[1,0],[1,1],[1,2],[0,2]],
+    [[0,0],[0,1],[1,1],[2,1]],
+    [[0,0],[0,1],[0,2],[1,0]],
+    [[0,0],[1,0],[2,0],[2,1]]
+  ],
+  t_shape: [
+    [[0,1],[1,0],[1,1],[2,1]],
+    [[1,0],[1,1],[1,2],[0,1]],
+    [[0,0],[1,0],[2,0],[1,1]],
+    [[0,0],[0,1],[0,2],[1,1]]
+  ],
+  line: [
+    [[0,0],[0,1],[0,2],[0,3]],
+    [[0,0],[1,0],[2,0],[3,0]]
+  ],
+  s_shape: [
+    [[1,0],[2,0],[0,1],[1,1]],
+    [[0,0],[0,1],[1,1],[1,2]],
+  ],
+  z_shape: [
+    [[0,0],[1,0],[1,1],[2,1]],
+    [[1,0],[0,1],[1,1],[0,2]],
+  ]
+}
 
 def self.boot args
   args.state = {}
@@ -77,13 +113,23 @@ end
 
 def self.tick_game
   unless $gg.lost_focus
-    spawn_falling_block unless $gg.falling_block
+    if any_loose_blocks?
+      $gg.loose_blocks = :true
+    else
+      $gg.loose_blocks = nil
+    end
+
+    unless $gg.loose_blocks
+      spawn_falling_block if $gg.falling_blocks.empty? unless $gg.blinking_shapes.any?
+    end
+
+    move_loose_blocks_down if $gg.loose_blocks
 
     if $inputs.keyboard.key_down.left
       move_block_left
     elsif $inputs.keyboard.key_down.right
       move_block_right
-    elsif $inputs.keyboard.key_down.down
+    elsif $inputs.keyboard.down
       move_block_down
     end
 
@@ -109,15 +155,26 @@ def self.set_block(x, y, block)
 end
 
 def self.random_block
-  { type: RESOURCE_TYPES.sample }
+  refill_bag_if_empty
+  type = $gg.bag.shift
+  { type: type }
+end
+
+def self.refill_bag_if_empty
+  if $gg.bag.empty?
+    $gg.bag = RESOURCE_TYPES.shuffle
+  end
 end
 
 def self.spawn_falling_block
-  $gg.falling_block = random_block.merge(x: (GRID_WIDTH / 2).floor, y: GRID_HEIGHT)
+  $gg.falling_blocks << random_block.merge(x: (GRID_WIDTH / 2).floor, y: GRID_HEIGHT)
+  #10.times do |i|
+  #  $gg.falling_blocks << random_block.merge(x: i, y: GRID_HEIGHT)
+  #end
 end
 
 def self.move_block_left
-  fb = $gg.falling_block
+  fb = $gg.falling_blocks.first
   return unless fb
 
   new_x = fb[:x] - 1
@@ -132,7 +189,7 @@ def self.move_block_left
 end
 
 def self.move_block_right
-  fb = $gg.falling_block
+  fb = $gg.falling_blocks.first
   return unless fb
 
   new_x = fb[:x] + 1
@@ -147,7 +204,31 @@ def self.move_block_right
 end
 
 def self.move_block_down
-  fb = $gg.falling_block
+  landed_blocks = []
+
+  $gg.falling_blocks.each do |fb|
+    next_y = fb[:y] - 0.5
+    next_y_floor = next_y.floor
+
+    if next_y < 0 || get_block(fb[:x], next_y_floor)
+      fb[:y] = fb[:y].floor
+      set_block(fb[:x], fb[:y], fb)
+      landed_blocks << fb
+    else
+      fb[:y] = next_y
+    end
+  end
+
+  landed_blocks.each do |fb|
+    $gg.falling_blocks.delete(fb)
+  end
+
+  check_for_flower_clusters unless landed_blocks.empty?
+end
+
+=begin
+def self.move_block_down
+  fb = $gg.falling_blocks.first
   return unless fb
 
   next_y = fb[:y] - 0.5
@@ -156,20 +237,21 @@ def self.move_block_down
   if next_y < 0 || get_block(fb[:x], next_y_floor)
     fb[:y] = fb[:y].floor
     set_block(fb[:x], fb[:y], fb)
-    $gg.falling_block = nil
+    $gg.falling_blocks.shift
     check_for_flower_clusters
   else
     fb[:y] = next_y
   end
 end
-
+=end
 def self.render_grid
-  $gg.grid.each_with_index do |block, i|
+  $gg.grid.each do |block|
     next unless block
-
+    if $gg.blinking_shapes.any? { |s| s[:blocks].include?(block) }
+      next
+    end
     x = block[:x] * TILE_SIZE + GRID_OFFSET_X
     y = block[:y] * TILE_SIZE + GRID_OFFSET_Y
-
     $outputs[:garden].sprites << {
       x: x, y: y,
       w: TILE_SIZE, h: TILE_SIZE,
@@ -177,11 +259,9 @@ def self.render_grid
     }
   end
 
-  if $gg.falling_block
-    fb = $gg.falling_block
+  $gg.falling_blocks.each do |fb|
     x = fb[:x] * TILE_SIZE + GRID_OFFSET_X
     y = fb[:y] * TILE_SIZE + GRID_OFFSET_Y
-
     $outputs[:garden].sprites << {
       x: x, y: y,
       w: TILE_SIZE, h: TILE_SIZE,
@@ -189,10 +269,124 @@ def self.render_grid
     }
   end
 
+  if $gg.blinking_shapes
+    $gg.blinking_shapes.each do |blink|
+      blink[:timer] -= 1
+      if blink[:timer] % blink[:blink_interval] == 0
+        blink[:visible] = !blink[:visible]
+      end
+
+      if blink[:visible]
+        blink[:blocks].each do |b|
+          x = b[:x] * TILE_SIZE + GRID_OFFSET_X
+          y = b[:y] * TILE_SIZE + GRID_OFFSET_Y
+          $outputs[:garden].sprites << {
+            x: x, y: y,
+            w: TILE_SIZE, h: TILE_SIZE,
+            path: "sprites/#{b[:type]}.png"
+          }
+        end
+      end
+    end
+
+    $gg.blinking_shapes.reject! do |blink|
+      if blink[:timer] <= 0
+        blink[:blocks].each { |b| set_block(b[:x], b[:y], nil) }
+        $gg.score += 10
+        $gg.camera_trauma = 0.5
+        true
+      else
+        false
+      end
+    end
+  end
+
   draw_grid_outline
 end
 
+def self.move_loose_blocks_down
+  return unless $gg.clock.zmod?(6)
+
+  loose_blocks = []
+  (0...GRID_HEIGHT).each do |y|
+    (0...GRID_WIDTH).each do |x|
+      b = get_block(x, y)
+      next unless b
+
+      below_y = b[:y] - 0.5
+      next if below_y < 0
+
+      below_floor = below_y.floor
+      below_block = get_block(x, below_floor)
+
+      loose_blocks << b if below_block.nil? || below_block.equal?(b)
+    end
+  end
+
+  return if loose_blocks.empty?
+  lowest_y = loose_blocks.map { |b| b[:y] }.min
+  lowest_loose = loose_blocks.select { |b| b[:y] == lowest_y }
+
+  lowest_loose.each do |b|
+    old_y = b[:y]
+    old_floor = old_y.floor
+
+    new_y = [old_y - 0.5, 0.0].max
+    new_floor = new_y.floor
+
+    if new_floor < old_floor
+      set_block(b[:x], old_floor, nil)
+      b[:y] = new_y
+      set_block(b[:x], new_floor, b)
+    else
+      b[:y] = new_y
+    end
+  end
+end
+
+def self.any_loose_blocks?
+  (0...GRID_HEIGHT).each do |y|
+    (0...GRID_WIDTH).each do |x|
+      block = get_block(x, y)
+      next unless block
+
+      below_y = y - 1
+      if below_y >= 0 && !get_block(x, below_y)
+        return true
+      end
+      if (block[:y] % 1) == 0.5
+        return true
+      end
+    end
+  end
+  false
+end
+
 def self.check_for_flower_clusters
+  $gg.blinking_shapes ||= []
+
+  GRID_WIDTH.times do |x|
+    GRID_HEIGHT.times do |y|
+      SHAPES.each do |shape_name, orientations|
+        orientations.each do |coords|
+          blocks = coords.map { |dx, dy| get_block(x + dx, y + dy) }
+          next if blocks.any?(&:nil?)
+          types = blocks.map { |b| b[:type] }.reject { |t| t == :rock }.sort
+          if types == RESOURCE_TYPES.reject { |t| t == :rock }.sort
+            unless $gg.blinking_shapes.any? { |s| (s[:blocks] & blocks).any? }
+              $gg.blinking_shapes << { blocks: blocks.dup, timer: 60, visible: true, blink_interval: 10 }
+            end
+          end
+          #types = blocks.map { |b| b[:type] }.sort
+          #if types == RESOURCE_TYPES.sort
+          #  unless $gg.blinking_shapes.any? { |s| (s[:blocks] & blocks).any? }
+          #    $gg.blinking_shapes << { blocks: blocks.dup, timer: 60, visible: true, blink_interval: 10 }
+          #  end
+          #end
+        end
+      end
+    end
+  end
 end
 
 def self.bootstrap
@@ -206,31 +400,30 @@ def self.bootstrap
     high_score: 0,
     total_score: 0,
     clock: 0,
-    lost_focus: true
+    lost_focus: true,
+    bag: [],
+    blinking_shapes: [],
+    falling_blocks: [],
+    moved_loose_blocks: [],
+    loose_blocks: nil
   }
-
   $gg.grid = Array.new(GRID_WIDTH * GRID_HEIGHT)
 end
 
 def self.draw_grid_outline
   border_thickness = 14
-
   left   = { x: GRID_OFFSET_X - border_thickness, y: GRID_OFFSET_Y, w: border_thickness, h: GRID_PIXEL_HEIGHT, path: :pixel, r: 100, g: 200, b: 100 }
   right  = { x: GRID_OFFSET_X + GRID_PIXEL_WIDTH, y: GRID_OFFSET_Y, w: border_thickness, h: GRID_PIXEL_HEIGHT, path: :pixel, r: 100, g: 200, b: 100 }
-  # top    = { x: GRID_OFFSET_X - border_thickness, y: GRID_OFFSET_Y + GRID_PIXEL_HEIGHT, w: GRID_PIXEL_WIDTH + border_thickness * 2, h: border_thickness, path: :pixel, r: 100, g: 200, b: 100 }
   bottom = { x: GRID_OFFSET_X - border_thickness, y: GRID_OFFSET_Y - border_thickness, w: GRID_PIXEL_WIDTH + border_thickness * 2, h: border_thickness, path: :pixel, r: 100, g: 200, b: 100 }
-
   $outputs[:garden].sprites << [left, right, bottom]
 end
 
 def self.screenshake
   return if $gg.camera_trauma == 0
-
   next_offset = 100 * $gg.camera_trauma**2
   $gg.camera_x_offset = next_offset.randomize(:sign, :ratio).round
   $gg.camera_y_offset = next_offset.randomize(:sign, :ratio).round
   $gg.camera_trauma *= 0.95
-
   if $gg.camera_trauma < 0.05
     $gg.camera_trauma = 0
     $gg.camera_x_offset = 0
@@ -245,17 +438,14 @@ end
 def self.tick_title
   $outputs[:garden].labels << {
     x: 1260 / 2, y: 500, font: "fonts/IndieFlower-Regular.ttf", size_px: 192, r: 100, g: 200, b: 100, text: "Walled Garden", anchor_x: 0.5
-}
-
+  }
   return if $gg.clock < 30
-
   if $inputs.keyboard.key_down.r && $inputs.keyboard.key_held.h
     $gtk.write_file "data/high_score.txt", "0"
     $gg.high_score = 0
     $gg.total_score = 0
-    $gtk.reset # $gtk.reset_next_time
+    $gtk.reset
   end
-
   if ($inputs.keyboard.key_up.truthy_keys.any? || $inputs.mouse.click)
     $gg.next_scene = :game
   end
@@ -273,9 +463,9 @@ def self.game_has_lost_focus?
       # putz "gained focus"
     end
   end
-  # $gg.focus_color = focus ? { r: 225, g: 50, b: 50 } : { r: 50, g: 225, b: 50 }
   $gg.lost_focus = focus
 end
 
 $gtk.disable_framerate_warning!
 $gtk.reset
+
